6f9d1bf3067e4040f3b9c441c5c11d82d5e6a578
skotperez <voraginebramante@gmail.com>
2015-01-08 20:21:15 +0100

    featured and news sections in home

--- index.php
+++ index.php
@@ -1,2 +1,40 @@
 <?php include('header.php'); ?>
+
+<section id="featured" class="row sec">
+	<header class="hidden"><h1>Teatro Tribueñe</h1></header>
+	<article class="featured-item col-sm-12">
+		<div class="featured-item-bg"style="background-image: url('images/home.img.01.png');">
+			<div class="featured-item-text">
+				<header>
+					<div class="featured-item-current pull-right text-up">En gira</div>
+					<div class="featured-item-date text-up">Del 24 de abril al 22 de junio</div>
+					<h3 class="featured-item-tit text-up">Bodas de sangre</h3>
+				</header>
+				<div class="featured-item-author">de Federico García Lorca</div>
+				<div class="featured-item-director">Dirección Irina Kouberskaya</div>
+			</div>
+		</div>
+	</article><!-- .featured-item -->
+</section><!-- #featured -->
+
+<section id="news" class="row sec">
+	<div class="col-sm-12">
+		<header><h2 class="sec-tit">Noticias</h2></header>
+		<article class="news-item">
+			<header>
+				<time datetime="2014-12-04">04/12/2014</time>
+				<h3 class="news-item-tit"><a href="">Paseillo de Hugo Pérez de la Pica</a></h3>
+			</header>
+			<div class="news-item-desc">Una celebración estertórica, la intrahistoria de los restos de la fiesta. Una visión nueva de los engranajes y esencias. El arte de torear y alrededores. Bailado, cantado y recitado. Lo majo, lo curro, lo manolo, lo flamenco dónde y cuándo se diluyeron.</div>
+		</article>
+		<article class="news-item">
+			<header>
+				<time datetime="2014-12-01">01/12/2014</time>
+				<h3 class="news-item-tit"><a href="">¡Este puente ven al teatro!</a></h3>
+			</header>
+			<div class="news-item-desc">Os esperamos en Tribueñe con: "Navegando por ideas escondidas" jueves a las 20h, "Bodas de sangre" viernes a las 20h y "Regreso al hogar" sábados y domingos a las 19h.</div>
+		</article>
+	</div>
+</section><!-- #news -->
+
 <?php include('footer.php'); ?>
