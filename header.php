1d3942168afa0a3fd0ab67499e62bc9107c63639
skotperez <voraginebramante@gmail.com>
2015-01-08 10:25:50 +0100

    main files and basic header

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html lang="es-ES">
+
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<title>Teatro Tribueñe</title>
+
+<link rel='stylesheet' href='css/bootstrap.min.css?ver=4.1' type='text/css' media='all' />
+<link rel='stylesheet' href='css/style.css' type='text/css' media='all' />
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>
+	<script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
+	<![endif]-->
+
+</head>
+<body>
+
+<nav id="pre-navbar" class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#hce-pre-navbar-collapse">
+				<span class="sr-only">Mostrar/Ocultar menú</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+		<div class="collapse navbar-collapse" id="hce-pre-navbar-collapse">
+			<ul id="navbar-main" class="nav navbar-nav">
+				<li><a href="">Producciones Tribueñe</a></li>
+				<li><a href="">Otras producciones</a></li>
+				<li><a href="">Campañas escolares</a></li>
+				<li><a href="">Clases y encuentros</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div class="container">
+	<header class="row">
+		<div id="pre-logo" class="col-sm-2">
+			<img src="images/logo.png" alt="Logo Teatro Tribueñe" />
+		</div>
+		<div id="pre-main" class="col-sm-10">
+			<div id="pre-main-tit" class="pull-right">Premio Ojo Crítico de Teatro RNE</div>
+			<div>Teatro Tribueñe</div>
+		</div>
+	</header>
