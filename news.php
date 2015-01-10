b2e9fb26744f4aa60fc40159f9cb42c0f31a54cb
skotperez <voraginebramante@gmail.com>
2015-01-10 17:16:11 +0100

    news template

---
+++ news.php
@@ -0,0 +1,38 @@
+<?php include('header.php'); ?>
+
+<section id="news" class="sec">
+	<header class="row text-right text-up"><h1 id="main-tit" class="col-sm-12">Noticias</h1></header>
+	<article class="row new-item">
+		<div class="col-md-3 col-sm-push-9 new-item-img"><a href="#"><img class="img-responsive" src="images/programacion.img.01.png" alt="Bodas de Sangre" /></a></div>
+		<div class="col-sm-9 col-sm-pull-3 new-item-text">
+			<header>
+				<time class="new-item-date" datetime="2014-12-30">30/12/2014</time>
+				<h2 class="new-item-tit text-up">Nace la Academia de las Artes Escénicas de España</h2>
+			</header>
+			<div class="list-item-desc">
+				<p>El Teatro y la Danza por fin cuentan con una institución propia.</p>
+			</div>
+		</div>
+	</article>
+	<article class="row new-item">
+		<div class="col-md-3 col-sm-push-9 new-item-img"><a href="#"><img class="img-responsive" src="images/programacion.img.02.png" alt="Bodas de Sangre" /></a></div>
+		<div class="col-sm-9 col-sm-pull-3 new-item-text">
+			<header>
+				<time class="new-item-date" datetime="2014-12-30">30/12/2014</time>
+				<h2 class="new-item-tit text-up">Regresamos a Tribueñe a partir del 24 de abril</h2>
+			</header>
+			<div class="list-item-desc">
+				<p>"LA CASA DE BERNARDA ALBA"<br />
+         Del 24 de abril al 20 de junio.<br />
+         Jueves y viernes a las 20:00h<br />
+         (15 y 16 de mayo no hay función)</p>
+         <p>"BODAS DE SANGRE"<br />
+         Del 26 de abril al 22 de junio.<br />
+         Sábados y domingos a las 19:00h<br />
+         (17 y 18 de mayo no hay función)</p>
+			</div>
+		</div>
+	</article>
+</section><!-- #news -->
+
+<?php include('footer.php'); ?>
