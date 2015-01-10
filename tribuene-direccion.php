480c60814e3f18791a4fe5c3964250b4768a423d
skotperez <voraginebramante@gmail.com>
2015-01-10 19:26:09 +0100

    basic direccion template

---
+++ tribuene-direccion.php
@@ -0,0 +1,38 @@
+<?php include('header.php'); ?>
+
+<section id="directors" class="row sec">
+	<header class="col-sm-12 text-right"><h1 id="main-tit"><span class="text-up">Compañía Tribueñe /</span> Dirección</h1></header>
+	<div class="col-sm-12">
+		<div class="director-intro">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut enim lectus. Curabitur convallis dolor a porttitor lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean facilisis viverra pharetra. Cras commodo, nisi vel dictum volutpat, dolor lectus tempor urna, sit amet faucibus mauris massa vel velit. Nam interdum justo eu ipsum posuere, non lacinia nisi faucibus. Ut ut nunc placerat, accumsan urna sit amet, accumsan nunc. Ut imperdiet ullamcorper massa eu malesuada. Sed eu hendrerit nunc. Fusce ut libero a metus tincidunt lacinia. Suspendisse consectetur aliquet turpis, sit amet efficitur mi tincidunt sed. Vivamus non ante libero. Phasellus arcu quam, lacinia eu lorem sit amet, semper feugiat leo.</p>
+		</div>
+		<article class="director">
+			<header><h2 class="director-tit">Irina Kouberskaya</h2></header>
+			<div class="director-desc">Breve curriculum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut enim lectus. Curabitur convallis dolor a porttitor lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean facilisis viverra pharetra. Cras commodo, nisi vel dictum volutpat, dolor lectus tempor urna, sit amet faucibus mauris massa vel velit. Nam interdum justo eu ipsum posuere, non lacinia nisi faucibus.</p>
+			<ul class="list-unstyled director-extra">
+				<li><a href="">Curriculum en PDF</a></li>
+				<li><a href="">+ Enalces</a></li>
+			</ul>
+			<div class="row director-gallery">
+				<figure class="col-md-2"><img class="img-responsive" src="images/programacion.img.01.png" alt="Image alternative text" /></figure>
+				<figure class="col-md-2"><img class="img-responsive" src="images/programacion.img.02.png" alt="Image alternative text" /></figure>
+				<figure class="col-md-2"><img class="img-responsive" src="images/programacion.img.03.png" alt="Image alternative text" /></figure>
+			</div>
+		</article>
+		<article class="director">
+			<header><h2 class="director-tit">Hugo Pérez de la Pica</h2></header>
+			<p>Breve curriculum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut enim lectus. Curabitur convallis dolor a porttitor lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean facilisis viverra pharetra. Cras commodo, nisi vel dictum volutpat, dolor lectus tempor urna, sit amet faucibus mauris massa vel velit. Nam interdum justo eu ipsum posuere, non lacinia nisi faucibus.</p>
+			<ul class="list-unstyled director-extra">
+				<li><a href="">Curriculum en PDF</a></li>
+				<li><a href="">+ Enalces</a></li>
+			</ul>
+			<div class="row director-gallery">
+				<figure class="col-md-2"><img class="img-responsive" src="images/programacion.img.01.png" alt="Image alternative text" /></figure>
+				<figure class="col-md-2"><img class="img-responsive" src="images/programacion.img.02.png" alt="Image alternative text" /></figure>
+				<figure class="col-md-2"><img class="img-responsive" src="images/programacion.img.03.png" alt="Image alternative text" /></figure>
+			</div>
+		</article>
+	</div>
+</section><!-- #tribuene -->
+
+<?php include('footer.php'); ?>
