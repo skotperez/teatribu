72e9392f9faf3aefab8875394438eb2876827c13
skotperez <voraginebramante@gmail.com>
2015-01-11 22:31:03 +0100

    single producciones template

--- producciones.php
+++ producciones.php
@@ -2,10 +2,10 @@
 
 <header class="row text-right text-up"><h1 id="main-tit" class="col-sm-12">Producciones Tribueñe</h1></header>
 <div class="row">
-	<div class="col-sm-7">
+	<div class="col-sm-8">
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra urna arcu, et hendrerit massa tempor maximus. Pellentesque ut nisl bibendum, aliquam ligula id, lobortis mauris. Maecenas sit amet nisi enim. Morbi et magna sed est ullamcorper interdum at ac est. Integer sit amet ullamcorper lorem, vitae rhoncus quam. Mauris eu tellus in sapien faucibus porta et ac elit. Nam scelerisque, sem in volutpat dapibus, orci tortor vehicula elit, non pulvinar ipsum tellus vitae nulla. Praesent vitae pharetra est. Morbi euismod dui non turpis bibendum aliquam. Mauris eu leo at ante sagittis luctus. Suspendisse non sem non odio feugiat posuere eget ac dui. Pellentesque at vestibulum dolor, vitae ornare risus. Aenean bibendum ultricies leo. Nunc accumsan enim non magna pulvinar fermentum. Ut placerat dolor ut felis ultricies, id varius arcu malesuada. Ut facilisis dui nec consectetur rutrum.</p>
 	</div>
-	<aside class="col-md-4 col-md-offset-1">
+	<aside class="col-md-4">
 		<ul class="producciones-list montserrat-red list-unstyled">
 			<li><a href="">Azules rejas del amor</a></li>
 			<li><a href="">Navegando por ideas escondidas</a></li>
---
+++ single-producciones.php
@@ -0,0 +1,38 @@
+<?php include('header.php'); ?>
+
+<div class="row text-right text-up"><div id="main-tit" class="col-sm-12">Producciones Tribueñe</div></div>
+<div class="row">
+	<article class="col-sm-8">
+		<figure class="produccion-img"><img class="img-responsive" src="images/producciones.01.png" alt="Donde mira el ruiseñor cuando cruje una rama" /></figure>
+		<header><h1 class="produccion-tit lato-500-white">Donde mira el ruiseñor cuando cruje una rama</h1></header>
+		<div class="produccion-data lato-500-white">
+			de Hugo Pérez y Mikhail Studyonov<br />
+			2013 - Teatro Español de Madrid
+		</div>
+		<p>Santo retrato de Gabriel y María en seis jornadas que hacen siete con el Glorioso epílogo a modo de Auto Sacramental.</p>
+		<p>Genero: Musical</p>
+		<h2 class="lato-300-red">Sinopsis</h2>
+		<p>La cotidianidad hecha tragedia y la tragedia elevada a su expresión poética, han sido los vectores de nuestro trabajo sobre el texto de Lorca. Concebimos esta obra como una acto sacerdotal, levantando el inmenso mundo simbólico de Lorca. Tratamos de ser consecuentes con los retos de este tiempo y asumir, como dijo el poeta, la geometría del único mapa genético grabado en la sangre de todos.</p>
+		<h2 class="lato-300-red">Género y duración</h2>
+		<p>Drama. 2:30 horas con descanso de 15 minutos</p>
+		<h2 class="lato-300-red">Reparto</h2>
+		<p>Ma Luisa Ga Budí (Madre) , Miguel Pérez-Muñoz (Novio), Matilde Juárez/Nereida San Martín (Novia), José Luis Sanz (Padre), Alejandra Navarro/Chelo Vivares (Criada), David García (Leonardo), Irene Polo (Mujer de Leonardo), María Barrionuevo (Suegra), Inma Barrionuevo (Vecina/Muerte), Tábata Cerezo (Muchacha/Luna), Eduardo Pérez de Carrera (voz bosque/mantras)</p>
+	</article>
+	<aside class="col-md-4">
+		<ul class="producciones-list montserrat-red list-unstyled">
+			<li><a href="">Azules rejas del amor</a></li>
+			<li><a href="">Navegando por ideas escondidas</a></li>
+			<li><a href="">Bodas de sangre</a></li>
+			<li class="active"><a href="">Donde mira el ruiseñor cuando cruje una rama</a></li>
+			<li><a href="">Ligazón</a></li>
+			<li><a href="">La casa de Bernarda Alba</a></li>
+			<li><a href="">El jardín de los cerezos</a></li>
+			<li><a href="">Por los ojos de Raquel Meller</a></li>
+			<li><a href="">Lo malo es que casi nadie escucha</a></li>
+			<li><a href="">El público</a></li>
+			<li><a href="">Planeta sacromonte</a></li>
+		</ul>
+	</aside>
+</div>
+
+<?php include('footer.php'); ?>
