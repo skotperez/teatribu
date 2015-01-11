f9603323cf900ac9140128684e45263ce823e85f
skotperez <voraginebramante@gmail.com>
2015-01-11 18:24:09 +0100

    actores template

---
+++ tribuene-actores.php
@@ -0,0 +1,31 @@
+<?php include('header.php'); ?>
+
+<div class="row text-right"><div id="main-tit" class="col-sm-12"><span class="text-up">Compañía Tribueñe /</span> Actores</div></div>
+<div class="row">
+	<article class="col-sm-7 single-actor">
+		<header><h1 class="director-tit">Miguel Ángel Mendo</h1></header>
+		<div class="director-desc">
+			<p>Breve curriculum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut enim lectus. Curabitur convallis dolor a porttitor lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean facilisis viverra pharetra. Cras commodo, nisi vel dictum volutpat, dolor lectus tempor urna, sit amet faucibus mauris massa vel velit. Nam interdum justo eu ipsum posuere, non lacinia nisi faucibus.</p>
+			<p>Actúa en:</p>
+			<ul class="list-unstyled director-extra">
+				<li><a href="">Regreso al hogar</a></li>
+				<li><a href="">El jardín de los cerezos</a></li>
+			</ul>
+	</article>
+	<aside class="col-md-4 col-md-offset-1 single-actores-rel">
+		<div class="row actor-rel">
+			<figure class="col-md-4"><img class="img-responsive" src="images/actores-01.png" alt="Miguel Ángel Mendo" /></figure>
+			<div class="col-md-8">Miguel Ángel Mendo<br />Actor</div>
+		</div>
+		<div class="row actor-rel">
+			<figure class="col-md-4"><img class="img-responsive" src="images/actores-01.png" alt="Miguel Ángel Mendo" /></figure>
+			<div class="col-md-8">Miguel Ángel Mendo<br />Actor</div>
+		</div>
+		<div class="row actor-rel">
+			<figure class="col-md-4"><img class="img-responsive" src="images/actores-01.png" alt="Miguel Ángel Mendo" /></figure>
+			<div class="col-md-8">Miguel Ángel Mendo<br />Actor</div>
+		</div>
+	</aside>
+</div>
+
+<?php include('footer.php'); ?>
