a61f1449feeaf277fd770a0ea1138c3b80df981c
angelagomezperea <agomezperea@gmail.com>
2015-01-15 14:16:38 +0100

    completada la info para content

--- single-programacion.php
+++ single-programacion.php
@@ -18,6 +18,29 @@
 			<p>La cotidianidad hecha tragedia y la tragedia elevada a su expresión poética, han sido los vectores de nuestro trabajo sobre el texto de Lorca. Concebimos esta obra como una acto sacerdotal, levantando el inmenso mundo simbólico de Lorca. Tratamos de ser consecuentes con los retos de este tiempo y asumir, como dijo el poeta, la geometría del único mapa genético grabado en la sangre de todos.</p>
 			<h2>Género y duración</h2>
 			<p>Drama. 2:30 horas con descanso de 15 minutos</p>
+			<h2>Reparto</h2>
+			<p>Mª Luisa Gª Budí (Madre) , Miguel Pérez-Muñoz (Novio), Matilde Juárez/Nereida San Martín
+			(Novia), José Luis Sanz (Padre), Alejandra Navarro/Chelo Vivares (Criada),
+			David García (Leonardo), Irene Polo (Mujer de Leonardo), María Barrionuevo (Suegra), Inma
+			Barrionuevo (Vecina/Muerte), Tábata Cerezo (Muchacha/Luna),
+			Eduardo Pérez de Carrera (voz bosque/mantras)</p>
+			<h2>Ficha Artística</h2>
+			<p>ESCENOGRAFÍA E ILUMINACIÓN:Eduardo Pérez de Carrera </br>
+			VESTUARIO:Ana Moreno</br>
+			ESPACIO SONORO:Miguel Pérez-Muñoz, Jesús Ortiz, Irina
+			Kouberskaya</br>
+			TÉCNICO DE ILUMINACIÓN Y SONIDO:Paula Sánchez</p>
+			<h2>Premios</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim sed nisi ut imperdiet. Vestibulum eu quam nulla. Phasellus turpis leo, pharetra vel augue in, hendrerit facilisis eros. Fusce vehicula lectus nec dui vestibulum consectetur. Suspendisse sit amet faucibus est, at sagittis tortor. Mauris tempus leo vitae vulputate tristique. Suspendisse sem justo, consectetur malesuada nisi a, gravida dictum ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et blandit ipsum. </p>
+			<h2>Críticas</h2>
+			<p.quote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim sed nisi ut imperdiet. Vestibulum eu quam nulla. Phasellus turpis leo, pharetra vel augue in, hendrerit facilisis eros. Fusce vehicula lectus nec dui vestibulum consectetur. Suspendisse sit amet faucibus est, at sagittis tortor. Mauris tempus leo vitae vulputate tristique. Suspendisse sem justo, consectetur malesuada nisi a, gravida dictum ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et blandit ipsum. </p>
+			<p.quote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim sed nisi ut imperdiet. Vestibulum eu quam nulla. Phasellus turpis leo, pharetra vel augue in, hendrerit facilisis eros. Fusce vehicula lectus nec dui vestibulum consectetur. Suspendisse sit amet faucibus est, at sagittis tortor. Mauris tempus leo vitae vulputate tristique. Suspendisse sem justo, consectetur malesuada nisi a, gravida dictum ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et blandit ipsum. </p>
+			
+			<h2.media>Vídeo Promocional</h2>
+			<h2.media>Galería de Imágenes</h2>
+			<a href="">enlace ejemplo cuerpo</a>
+			<a href="">enlace ejemplo cuerpo</a>
+			
 		</div>
 		<aside class="col-sm-4">
 			<ul class="list-unstyled single-extra">
